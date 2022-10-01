95f5683e491bf70bd0ea02be3889d3cfcd3d4476
NicholasYang21 <1011268631@qq.com>
2022-10-01 18:28:19 +0800

    start to build a generator

--- src/codegen/codegen.rs
+++ src/codegen/codegen.rs
@@ -1,25 +1,27 @@
-use inkwell::builder::Builder;
 use inkwell::context::Context;
+/*use inkwell::types::AnyType;
+use inkwell::values::AnyValue;
 
 use crate::compiler::firelang_parser::ast::node::Expression;
-use crate::compiler::firelang_parser::ast::token::Literal;
+use crate::compiler::firelang_parser::ast::token::Literal;*/
 
-struct LLVMGenerator {
+struct _LLVMGenerator {
     ctx: Context,
 }
 
-impl LLVMGenerator {
-    pub fn new() -> Self {
-        LLVMGenerator {
+impl _LLVMGenerator {
+    pub fn _new() -> Self {
+        _LLVMGenerator {
             ctx: Context::create()
         }
     }
 
-    fn gen_expr(&mut self, expr: &Expression) {
+    /*fn gen_expr(&mut self, expr: &Expression) -> Box<dyn AnyValue> {
         match expr {
             Expression::Literal(x) => {
                 match x {
                     Literal::Int(val) => {
+                        Box::from(self.ctx.i32_type().const_int(*val as u64, true))
                     }
 
                     Literal::UInt(val) => {
@@ -38,5 +40,5 @@ impl LLVMGenerator {
 
             }
         }
-    }
+    }*/
 }
