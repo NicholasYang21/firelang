ccf8e1eb65e756b1b1450b01cb9c8fb48af03c80
NicholasYang21 <1011268631@qq.com>
2022-10-01 17:06:32 +0800

    start to build a generator

--- src/codegen/codegen.rs
+++ src/codegen/codegen.rs
@@ -1,2 +1,42 @@
-use crate::compiler::firelang_parser::parser::Parser;
+use inkwell::builder::Builder;
+use inkwell::context::Context;
 
+use crate::compiler::firelang_parser::ast::node::Expression;
+use crate::compiler::firelang_parser::ast::token::Literal;
+
+struct LLVMGenerator {
+    ctx: Context,
+}
+
+impl LLVMGenerator {
+    pub fn new() -> Self {
+        LLVMGenerator {
+            ctx: Context::create()
+        }
+    }
+
+    fn gen_expr(&mut self, expr: &Expression) {
+        match expr {
+            Expression::Literal(x) => {
+                match x {
+                    Literal::Int(val) => {
+                    }
+
+                    Literal::UInt(val) => {
+
+                    }
+
+                    _ => { unimplemented!() }
+                }
+            }
+
+            Expression::Ident(x) => {
+
+            }
+
+            Expression::Binary { lhs, op, rhs } => {
+
+            }
+        }
+    }
+}
