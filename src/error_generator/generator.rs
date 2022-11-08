a20da981a3b0c1f2eb46fb2bca975744c0336abf
NicholasYang21 <1011268631@qq.com>
2022-11-08 17:13:56 +0800

    .

---
+++ src/error_generator/generator.rs
@@ -0,0 +1,42 @@
+use std::convert::Into;
+use std::fmt::{Display, Formatter};
+use std::ops::Range;
+
+pub struct FireError {
+    messages: Vec<String>
+}
+
+pub enum Type {
+    Warning,
+    Error,
+}
+
+impl Display for FireError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let mut message: String = "".into();
+        for s in self.messages {
+            message.push_str((s + "\n").as_str());
+        };
+
+        write!(f, "{}", message)
+    }
+}
+
+impl FireError {
+    fn new(ty: Type) -> FireError {
+        let mut message: String = "".into();
+
+        match ty {
+            Type::Error => message += "Error",
+            Type::Warning => message += "Warning",
+        }
+
+        FireError {
+            messages: vec![message]
+        }
+    }
+
+    fn stress(line: u32, range: Range<u32>) -> &mut FireError {
+
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ extern crate core;
 
 pub mod codegen;
 pub mod compiler;
+pub mod error_generator;
