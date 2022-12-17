4ab25607636d23f39f7363981bb740f8eae46b98
NicholasYang21 <1011268631@qq.com>
2022-12-17 22:25:58 +0800

    Do some startup work for IR code generator.

--- src/vm/codegen.rs
+++ src/vm/codegen.rs
@@ -1,7 +1,29 @@
-pub struct ByteCode {
+use crate::compiler::firelang_parser::ast::node::*;
 
+enum OpType {
+    Add,
+    Sub,
+    Mul,
+}
+
+pub struct ByteCode {
+    opcode: OpType
 }
 
 pub trait Generator {
     fn gen(&mut self) -> ByteCode;
+}
+
+impl Generator for Expression {
+    fn gen(&mut self) -> ByteCode {
+        match &self {
+            Expression::Binary { lhs, op, rhs} => {
+
+            }
+
+            _ => (),
+        }
+
+        unimplemented!()
+    }
 }
